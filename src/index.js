6953031ec1a454b0289649f9decb1592ede36f80
gagagagagagag <jakubprzywara@icloud.com>
2019-11-15 19:54:31 +0100

    Init commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const chalk = require("chalk");
+const path = require("path");
+const hbs = require("hbs");
+
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+const app = express();
+
+app.use(express.json());
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirPath));
+
+app.get("/", (req, res) => {
+   res.render("index");
+});
+
+app.get("/*", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log();
+    console.log(chalk.green("The server is listening on port: " + process.env.PORT));
+    console.log();
+});
